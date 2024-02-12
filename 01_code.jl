f3e85045ea0c9809469b391efa06b99f7674bc05
Tanya Strydom <tanya.strydom@icloud.com>
2024-02-12 15:48:45 +0000

    :sparkles: added functionality for cascade model

--- 01_code.jl
+++ 01_code.jl
@@ -1,10 +1,11 @@
 using Distributions
 using Mangal
+using Random
 using SpeciesInteractionNetworks
 
 # # Niche Model
 
 niche_network = structuralmodel(NicheModel, 10, 0.2)
-niche_network.edges
+cascade_network = cascademodel(10, 0.2)
 
 adbmm = adbmmodel(10, rand(Float64, 10), rand(Float64, 10))
---
+++ lib/cascade.jl
@@ -0,0 +1,58 @@
+"""
+cascademodel(species::Int64, connectance::Float64)
+
+Return matrix of randomly assembled interactions according to
+the cascade model for a specifiec number of species and 
+connectance.
+
+Note this function has been ported from the EcologigalNetworks.jl
+source code and has been modified for the purpose of this project.
+
+
+#### References
+
+Cohen, J.E., Newman, C.M., 1985. A stochastic theory of community food webs I.
+Models and aggregated data. Proceedings of the Royal Society of London. Series
+B. Biological Sciences 224, 421–448. https://doi.org/10.1098/rspb.1985.0042
+
+Poisot, Timothée, Zacharie Belisle, Laura Hoebeke, Michiel Stock, and Piotr
+Szefer. 2019. “EcologicalNetworks.jl - Analysing Ecological Networks.” Ecography.
+https://doi.org/10.1111/ecog.04310.
+"""
+function cascademodel(species::Int64, connectance::Float64)
+
+    @assert 0.0 < connectance < 0.5
+
+    # Initiate matrix
+    edges = zeros(Bool, (species, species))
+
+    # For each species randomly asscribe rank e
+    e = Random.sort(rand(species); rev=false)
+
+    # Probability for linking two species
+    p = 2*connectance*species/(species - 1)
+
+    for consumer in  1:species
+
+        # Rank for a consumer
+        rank = e[consumer]
+
+        # Get a set of resources smaller than consumer
+        potentialresources = findall(x -> x > rank, e)  # indices
+
+        # Each gets a link with a probability p
+        for resource in potentialresources
+
+            # Take the resources and for each of them check a random number
+            # if it is smaller than probability of linking two speceis create
+            # a link in the A matrix
+
+            # Random number for a potential resource
+            rand() < p && (edges[consumer, resource] = true)
+        end
+    end
+
+    edges = Binary(edges)
+    nodes = Unipartite(edges)
+    return SpeciesInteractionNetworks.SpeciesInteractionNetwork(nodes, edges)
+end
