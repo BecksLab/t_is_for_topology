3e9f9459db5631f118ffa1f3f88cf29a080a752c
Tanya Strydom <tanya.strydom@icloud.com>
2024-02-16 12:44:59 +0000

    :sparkles: update data structure

--- 00_mangal_networks.jl
+++ 00_mangal_networks.jl
@@ -3,29 +3,17 @@ using DataFrames
 using Mangal
 using SpeciesInteractionNetworks
 
-network_count = 3
+network_count = 10
 mangal_networks = Mangal.networks("type" => "herbivory", "count" => network_count)
 # 20 datasets for now just for initial testing
 
 # make a nice 'dataframe' to store network data
-mutable struct MangalNetworks
-    id::Vector{Int64}
-    richness::Vector{Int64}
-    links::Vector{Int64}
-    connectance::Vector{Float64}
-    # we can add some 'summary stats' here as well
-end
-
-function MangalNetworks(C::Int64)
-    return MangalNetworks(
-        zeros(Int64, C),
-        zeros(Int64, C),
-        zeros(Int64, C),
-        zeros(Float64, C),
-    )
-end
-
-mangal_topology = MangalNetworks(network_count); 
+mangal_topology = DataFrame(
+    id = zeros(Int64, network_count),
+    richness = zeros(Int64, network_count),
+    links = zeros(Int64, network_count),
+    connectance = zeros(Float64, network_count)
+); 
 
 for i in eachindex(mangal_networks)
     N = simplify(mangalnetwork(mangal_networks[i].id))
@@ -36,11 +24,4 @@ for i in eachindex(mangal_networks)
     mangal_topology.connectance[i] = connectance(N)
 end
 
-df = DataFrame(
-    id = mangal_topology.id,
-    richness = mangal_topology.richness,
-    links = mangal_topology.links,
-    connectance = mangal_topology.connectance
-)
-
-CSV.write("data/mangal_summary.csv", df)
+CSV.write("data/mangal_summary.csv", mangal_topology)
