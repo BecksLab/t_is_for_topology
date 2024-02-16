a5ef1b1e23072b45d8a0e4aa1fece57571ad1a5d
Tanya Strydom <tanya.strydom@icloud.com>
2024-02-16 12:54:40 +0000

    🏗️ topology FRAMEWORK (not results... yet)

--- 01_code.jl
+++ 01_code.jl
@@ -1,4 +1,5 @@
-#using DataFrames
+using CSV
+using DataFrames
 using Distributions
 #using EcologicalNetworks
 using Mangal
@@ -10,12 +11,17 @@ include("lib/adbm.jl")
 include("lib/cascade.jl")
 #include("lib/nestedhierarchy.jl")
 
-species = 10
-Co = 0.2
+# create new columns for models
+topology = CSV.read("data/mangal_summary.csv", DataFrame)
+insertcols!(topology, 5, :niche_links => zeros(Int64, nrow(topology)))
+insertcols!(topology, 6, :cascade_links => zeros(Int64, nrow(topology)))
 
-niche_network = structuralmodel(NicheModel, species, Co)
-cascade_network = cascademodel(species, Co)
-hierarchy_network = nestedhierarchymodel(species, 50)
+# calcualte links (for now) for relevant models
+for i in 1:nrow(topology)
+    
+    topology.niche_links[i] = links(structuralmodel(NicheModel, topology.richness[i], topology.connectance[i]))
+    topology.cascade_links[i] = links(cascademodel(topology.richness[i], topology.connectance[i]))
+    
+end
 
-adbmm = adbmmodel(species, rand(Float64, 10), rand(Float64, 10))
-
+CSV.write("data/topology_summary.csv", topology)
