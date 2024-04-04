89810e859fdda74c69b5764919b7106e27bb903a
Tanya Strydom <tanya.strydom@icloud.com>
2024-04-04 15:31:40 +0100

    🏗️ change up the workflow AND use null model instead of erdos_renyi

--- 00_mangal_networks.jl
+++ 00_mangal_networks.jl
@@ -11,46 +11,49 @@ mangal_networks = Mangal.networks("type" => "herbivory", "count" => network_coun
 
 # make a nice 'dataframe' to store network data
 mangal_topology = DataFrame(
-    id = zeros(Int64, network_count),
-    richness = zeros(Int64, network_count),
-    links = zeros(Int64, network_count),
-    connectance = zeros(Float64, network_count),
-    complexity = zeros(Float64, network_count),
-    distance = zeros(Float64, network_count),
-    basal = zeros(Float64, network_count),
-    top = zeros(Float64, network_count),
-    S1 = zeros(Float64, network_count),
-    S2 = zeros(Float64, network_count),
-    S4 = zeros(Float64, network_count),
-    S5 = zeros(Float64, network_count),
+    id = Int64[],
+    richness = Int64[],
+    links = Int64[],
+    connectance = Float64[],
+    complexity = Float64[],
+    distance = Float64[],
+    basal = Float64[],
+    top = Float64[],
+    S1 = Float64[],
+    S2 = Float64[],
+    S4 = Float64[],
+    S5 = Float64[],
 ); 
 
 @showprogress for i in eachindex(mangal_networks)
     N = simplify(mangalnetwork(mangal_networks[i].id))
     N = render(Binary, N) # make binary
 
-    gen = SpeciesInteractionNetworks.generality(N)
-    ind_maxgen = findmax(collect(values(gen)))[2]
-    basal = findall(x -> x == 0.0, collect(values(gen))) # find species with generality of zero
-
-    vul = SpeciesInteractionNetworks.vulnerability(N)
-    top = findall(x -> x == 0.0, collect(values(vul))) # find species with vulnerability of zero
-
-    mangal_topology.id[i] = mangal_networks[i].id
-    mangal_topology.richness[i] = richness(N)
-    mangal_topology.links[i] = links(N)
-    mangal_topology.connectance[i] = connectance(N)
-    mangal_topology.complexity[i] = complexity(N)
-    mangal_topology.distance[i] = distancetobase(N, collect(keys(gen))[ind_maxgen])
-    mangal_topology.basal[i] = length(basal)
-    mangal_topology.top[i] = length(top)
-    mangal_topology.S1[i] = length(findmotif(motifs(Unipartite, 3)[1], N))
-    mangal_topology.S2[i] = length(findmotif(motifs(Unipartite, 3)[2], N))
-    mangal_topology.S4[i] = length(findmotif(motifs(Unipartite, 3)[4], N))
-    mangal_topology.S5[i] = length(findmotif(motifs(Unipartite, 3)[5], N))
+    if richness(N) <= 200 # remove large networks because they make things slow
+        gen = SpeciesInteractionNetworks.generality(N)
+        ind_maxgen = findmax(collect(values(gen)))[2]
+        basal = findall(x -> x == 0.0, collect(values(gen))) # find species with generality of zero
+    
+        vul = SpeciesInteractionNetworks.vulnerability(N)
+        top = findall(x -> x == 0.0, collect(values(vul))) # find species with vulnerability of zero
+    
+        D = Dict{Symbol, Any}()
+            D[:id] = mangal_networks[i].id
+            D[:richness] = richness(N)
+            D[:links] = links(N)
+            D[:connectance] = connectance(N)
+            D[:complexity] = complexity(N)
+            D[:distance] = distancetobase(N, collect(keys(gen))[ind_maxgen])
+            D[:basal] = length(basal)
+            D[:top] = length(top)
+            D[:S1] = length(findmotif(motifs(Unipartite, 3)[1], N))
+            D[:S2] = length(findmotif(motifs(Unipartite, 3)[2], N))
+            D[:S4] = length(findmotif(motifs(Unipartite, 3)[4], N))
+            D[:S5] = length(findmotif(motifs(Unipartite, 3)[5], N))
+        push!(mangal_topology, D)
+    end
 end
-
-CSV.write("data/mangal_summary.csv", mangal_topology)
 
 ## Write file
 CSV.write("data/mangal_networks.csv", mangal_networks)
+CSV.write("data/mangal_summary.csv", mangal_topology)
--- 01_code.jl
+++ 01_code.jl
@@ -19,6 +19,7 @@ include("lib/random.jl")
 
 # import mangal networks
 mangal_topology = CSV.read("data/mangal_summary.csv", DataFrame)
+mangal_networks = DataFrame(CSV.File("data/mangal_networks.csv"))
 
 # create df for the outputs to be stored (long format)
 
@@ -38,86 +39,58 @@ topology  = DataFrame(
 
 ## Structural networks
 
-model_names = ["random", "niche", "cascade", "hierarchy"]
+model_names = ["random", "niche", "cascade", "hierarchy", "maxent"]
 n_reps = 10 #number of reps for each model for each network
 
 for _ in 1:n_reps
     @showprogress for i in 1:(nrow(mangal_topology))
         
+        mangal_network = simplify(mangalnetwork(mangal_networks.id[i]))
+        mangal_network = render(Binary, mangal_network)
+
         for (j, val) in enumerate(model_names)
             if val == "random"
-                N = randommodel(mangal_topology.richness[i], mangal_topology.links[i])
+                N = nullmodel(Connectance, mangal_network)
+                N = randomdraws(N)
             elseif val == "niche"
                 N = structuralmodel(NicheModel, mangal_topology.richness[i], mangal_topology.connectance[i])
             elseif val == "cascade"
                 N = cascademodel(mangal_topology.richness[i], mangal_topology.connectance[i])
-            else val == "hierarchy"
+            elseif val == "hierarchy"
                 N = nestedhierarchymodel(mangal_topology.richness[i], mangal_topology.links[i])
+            else val == "maxent"
+                N = maxentmodel(mangal_network;
+                # ❗ TODO
+                nchains = 2,
+                nsteps = 20)
             end
+        
+            N = simplify(N)
+            N = render(Binary, N)
+        
+            gen = SpeciesInteractionNetworks.generality(N)
+            ind_maxgen = findmax(collect(values(gen)))[2]
+            basal = findall(x -> x == 0.0, collect(values(gen)))
 
-        N = simplify(N)
-            
-        gen = SpeciesInteractionNetworks.generality(N)
-        ind_maxgen = findmax(collect(values(gen)))[2]
-        basal = findall(x -> x == 0.0, collect(values(gen)))
-
-        vul = SpeciesInteractionNetworks.vulnerability(N)
-        top = findall(x -> x == 0.0, collect(values(vul))) # find species with vulnerability of zero
-    
-        D = Dict{Symbol, Any}()
-            D[:id] = mangal_topology.id[i]
-            D[:model] = val
-            D[:connectance_mod] = connectance(N)
-            D[:complexity_mod] = complexity(N)
-            D[:distance_mod] = distancetobase(N, collect(keys(gen))[ind_maxgen])
-            D[:basal_mod] = length(basal)
-            D[:top_mod] = length(top)
-            D[:S1_mod] = length(findmotif(motifs(Unipartite, 3)[1], N))
-            D[:S2_mod] = length(findmotif(motifs(Unipartite, 3)[2], N))
-            D[:S4_mod] = length(findmotif(motifs(Unipartite, 3)[4], N))
-            D[:S5_mod] = length(findmotif(motifs(Unipartite, 3)[5], N))
+            vul = SpeciesInteractionNetworks.vulnerability(N)
+            top = findall(x -> x == 0.0, collect(values(vul))) # find species with vulnerability of zero
+     
+            D = Dict{Symbol, Any}()
+                D[:id] = mangal_topology.id[i]
+                D[:model] = val
+                D[:connectance_mod] = connectance(N)
+                D[:complexity_mod] = complexity(N)
+                D[:distance_mod] = distancetobase(N, collect(keys(gen))[ind_maxgen])
+                D[:basal_mod] = length(basal)
+                D[:top_mod] = length(top)
+                D[:S1_mod] = length(findmotif(motifs(Unipartite, 3)[1], N))
+                D[:S2_mod] = length(findmotif(motifs(Unipartite, 3)[2], N))
+                D[:S4_mod] = length(findmotif(motifs(Unipartite, 3)[4], N))
+                D[:S5_mod] = length(findmotif(motifs(Unipartite, 3)[5], N))
             push!(topology, D)
         end  
     end
 end
-
-## MaxEnt networks
-
-mangal_networks = DataFrame(CSV.File(joinpath("data", "mangal_networks.csv")))
-
-for _ in 1:n_reps
-    @showprogress for i in 1:(nrow(mangal_networks))
-
-        mangal_network = simplify(mangalnetwork(mangal_networks.id[i]))
-        
-        N = maxentmodel(mangal_network;
-        # ❗ TODO
-        nchains = 2,
-        nsteps = 20)
-            
-        gen = SpeciesInteractionNetworks.generality(N)
-        ind_maxgen = findmax(collect(values(gen)))[2]
-        basal = findall(x -> x == 0.0, collect(values(gen)))
-
-        vul = SpeciesInteractionNetworks.vulnerability(N)
-        top = findall(x -> x == 0.0, collect(values(vul))) # find species with vulnerability of zero
-        
-        D = Dict{Symbol, Any}()
-            D[:id] = mangal_topology.id[i]
-            D[:model] = "maxent"
-            D[:connectance_mod] = connectance(N)
-            D[:complexity_mod] = complexity(N)
-            D[:distance_mod] = distancetobase(N, collect(keys(gen))[ind_maxgen])
-            D[:basal_mod] = length(basal)
-            D[:top_mod] = length(top)
-            D[:S1_mod] = length(findmotif(motifs(Unipartite, 3)[1], N))
-            D[:S2_mod] = length(findmotif(motifs(Unipartite, 3)[2], N))
-            D[:S4_mod] = length(findmotif(motifs(Unipartite, 3)[4], N))
-            D[:S5_mod] = length(findmotif(motifs(Unipartite, 3)[5], N))
-            push!(topology, D)
-        end
-    end 
-
 
 ## Neutral networks
 
