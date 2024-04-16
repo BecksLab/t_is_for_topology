1fd818c312d7feb71a6bdac43bf1f3ef933472c8
Tanya Strydom <tanya.strydom@icloud.com>
2024-04-16 12:00:16 +0100

    📝 documentation

--- lib/functions/neutral.jl
+++ lib/functions/neutral.jl
@@ -16,6 +16,10 @@ neutralmodel(abundance::Vector, L::Int64, species_list::Vector{String})
     “What Constrains Food Webs? A Maximum Entropy Framework for Predicting 
     Their Structure with Minimal Biases.” PLOS Computational Biology 19 (9): 
     e1011458. https://doi.org/10.1371/journal.pcbi.1011458.
+
+    Pomeranz, Justin P. F., Ross M. Thompson, Timothée Poisot, and Jon S. Harding. 2019.
+    “Inferring Predator–Prey Interactions in Food Webs.” Methods in Ecology and Evolution
+    10 (3): 356–67. https://doi.org/10.1111/2041-210X.13125.
 """
 function neutralmodel(abundance::Vector, L::Int64, species_list::Vector{String})
     rel_abun = abundance ./ sum(abundance) # vector of relative abundances
